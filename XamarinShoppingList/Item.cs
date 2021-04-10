9e00e99040a8440afec6580dce4d377afb0a6b2c
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-04-10 09:49:04 +0300

    listing from cloud works

---
+++ XamarinShoppingList/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XamarinShoppingList
+{
+    public partial class Item
+    {
+        public DateTime TimeAdded { get; set; }
+        public string ItemName { get; set; }
+        public int? Pieces { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ XamarinShoppingList/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// Get requests: https://shoppingbackendapi.azurewebsites.net/api/shoppinglist/code/handyshopper
+
+namespace XamarinShoppingList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadDataFromRestAPI();
+
+
+            async void LoadDataFromRestAPI()
+            {
+                dataLista.ItemsSource = new List<string> { "Ladataan", "Ostoslista", "palvelimelta..." };
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://shoppingbackendapi.azurewebsites.net/");
+                string json = await client.GetStringAsync("/api/shoppinglist/code/handyshopper");
+                IEnumerable<Item> raakaLista = JsonConvert.DeserializeObject<Item[]>(json);
+
+                List<String> stringList = new List<string>();
+
+                foreach (var item in raakaLista)
+                {
+                    string merkkijono = item.ItemName.ToString() +
+                  " " + item.Pieces.ToString() + " kpl";
+                    stringList.Add(merkkijono);
+                }
+
+                dataLista.ItemsSource = stringList;
+
+            }
+        }
+
+        private void kerätty_nappi_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
